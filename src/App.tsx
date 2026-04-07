650d2795b67a5caf06fa01b4d21e0ed7cd139cd4
kxuak <kauad4867@gmail.com>
2026-04-07 15:48:15 -0300

    bora la

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      sexo off
+    <div className='bg-red-500'>sexo</div>
+
     </>
   )
 }
